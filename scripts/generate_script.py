309d4ec93b5298c3b3e5399f83dcb7ba8449ca60
Ujwal Anand <anandujwal6@gmail.com>
2026-02-07 10:17:45 +0530

    Update generate_script.py

--- scripts/generate_script.py
+++ scripts/generate_script.py
@@ -1,1 +1,11 @@
+import datetime
 
+now = datetime.datetime.utcnow().isoformat()
+
+print("✅ GitHub Actions is working")
+print("🕒 Time (UTC):", now)
+
+with open("test_output.txt", "w") as f:
+    f.write(f"Pipeline ran successfully at {now}\n")
+
+print("📄 test_output.txt file created")
